1da315e5cc9d0a021cc4816e7ac448a9050cd5cc
simon folkerts <simfolkerts@gmail.com>
2016-12-05 16:13:01 +1300

    main site pretty much complete. README complete

--- documentation/README.md
+++ documentation/README.md
@@ -1,2 +1,23 @@
 # Mellor-Cinemas
 Website project for server side scripting assignment.
+
+BOOKING
+
+Users can browse the front page and view different movie sessions. 
+A session can be selected, upon which the user is either taken to the booking page, or is asked to log in/create an account if they haven't already.
+The booking page summarises the movie, and prompts the user to choose a seat or multiple seats and confirm. The seat display only allows the selection of unreserved seats.
+
+ACCOUNT
+
+Users can go to the account page to see their account details and to view any bookings currently active. The options to delete or edit the account are here.
+Account deletion should remove any bookings and junctions to seats, entirely erasing the link.
+
+ADMIN
+
+Administrators can log in to the administrator role using the correct credentials.
+
+username: administrator
+password: admin
+
+Here the admin can pull up lists of movies and users. The admin can choose to show all showings for a given movie, or show all bookings for a given user.
+Movies and showings can be edited or deleted (providing no active dependents), and users and bookings can be deleted.
--- page-view/login-view.php
+++ page-view/login-view.php
@@ -11,7 +11,7 @@
                         <label for="username">User Name: </label>
                         <input type="text" name="login-username" <?php if(isset($_POST['login-username'])) { echo 'value=' . Utilities::escape($_POST['login-username']); } ?>><br>
                         <label for="password">Password: </label>
-                        <input type="text" name="login-password" <?php if(isset($_POST['login-password'])) { echo 'value=' . Utilities::escape($_POST['login-password']); } ?>><br>
+                        <input type="password" name="login-password" <?php if(isset($_POST['login-password'])) { echo 'value=' . Utilities::escape($_POST['login-password']); } ?>><br>
                     </div>
                     <div>
                         <button class="button" type="submit" name="submit" value="submit">Log In</button>
@@ -29,7 +29,7 @@
                         <label for="username">User Name: </label>
                         <input type="text" name="create-username" <?php if(isset($_POST['create-username'])) { echo 'value=' . Utilities::escape($_POST['create-username']); } ?>><br>
                         <label for="password">Password: </label>
-                        <input type="text" name="create-password" <?php if(isset($_POST['create-password'])) { echo 'value=' . Utilities::escape($_POST['create-password']); } ?>><br>
+                        <input type="password" name="create-password" <?php if(isset($_POST['create-password'])) { echo 'value=' . Utilities::escape($_POST['create-password']); } ?>><br>
                         <label for="email">Email:</label>
                         <input type="text" name="create-email" <?php if(isset($_POST['create-email'])) { echo 'value=' . Utilities::escape($_POST['create-email']); } ?>><br>
                     </div>
